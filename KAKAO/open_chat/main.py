349fc9e7af9bab15531f38fc1ca953048cac0587
GwangYoung <gospotb1607@gmail.com>
2022-09-05 15:46:43 +0900

    List를 dict처럼 사용할 수는 없는 걸까..?

--- KAKAO/open_chat/main.py
+++ KAKAO/open_chat/main.py
@@ -1,5 +1,3 @@
-# TODO 1: 채팅방에 누군가 들어오면 다음 메시지가 출력된다. "[닉네임]님이 들어왔습니다."
-# TODO 2: 채팅방에서 누군가 나가면 다음 메시지가 출력된다. "[닉네임]님이 나갔습니다."
 # TODO 3: 채팅방에서 닉네임을 변경하는 방법은 다음과 같이 두 가지이다.
 #  채팅방을 나간 후, 새로운 닉네임으로 다시 들어간다.
 #  채팅방에서 닉네임을 변경한다.
@@ -9,8 +7,9 @@
 #  문자열 배열 형태로 return 하도록 solution 함수를 완성하라.
 # # herf = https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=python3
 
-RECORD = ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]
-user_data = {}
+
+RECORDS = ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]
+log_data = {}
 
 
 def chat_output(user_log):
@@ -19,24 +18,29 @@ def chat_output(user_log):
 
 
 def enter_chat(user_log):
-    '''유저 아이디를 인식해서 출력해줌.'''
+    '''유저가 채팅방에 들어왔을 때, 아이디를 인식해서 들어왔다고 출력해줌.'''
 
     data_split = user_log.split()
 
     user_id = data_split[1]
     user_name = data_split[2]
-
-    user_data[f"{user_id}"] = user_name
-    string_print = f"{user_data[f'{user_id}']}님이 들어왔습니다."
+    if user_id in log_data.keys() and user_name != log_data[user_id]: # 유저 로그에 id가 존재하지만, 이름이 다르면 -> log_data 안에 있는 이름 교체
+        pass
+    log_data[f"{user_id}"] = user_name
+    string_print = f"{log_data[f'{user_id}']}님이 들어왔습니다."
 
     return string_print
 
 
 def out_chat(user_log):
+    '''유저가 채팅방에 들어왔을 때, 아이디를 인식해서 나갔다고 출력해줌.'''
     data_split = user_log.split()
 
     user_id = data_split[1]
-    user_name = data_split[2]
+
+    string_print = f"{log_data[f'{user_id}']}님이 나갔습니다."
+
+    return string_print
 
 
 def change_chat(user_log):
@@ -45,20 +49,23 @@ def change_chat(user_log):
     user_id = data_split[1]
     user_name = data_split[2]
 
+    log_data[f"{user_id}"] = user_name
 
 
 def solution(record):
     answer = []
     for r in record:
         if "Enter" in r:
-            enter_chat(r)
+            answer.append(enter_chat(r))
         elif "Leave" in r:
-            out_chat(r)
+            answer.append(out_chat(r))
         elif "Change" in r:
             change_chat(r)
-    print(f"user_data: {user_data}\n\n")
+    print(f"user_data: {log_data}\n\n")
+
     return answer
 
 
-print(solution(RECORD))
+print(f"Record : {solution(RECORDS)}")
 
+# "Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."
