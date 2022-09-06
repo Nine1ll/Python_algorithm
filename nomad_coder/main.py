7248332a92a8206c19e79a7275baa65686d2ed52
GwangYoung <gospotb1607@gmail.com>
2022-09-06 22:45:06 +0900

    nomad coder day 7

---
+++ nomad_coder/main.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def extract_jobs(term):
+    url = f"https://remoteok.com/remote-{term}-jobs"
+    request = requests.get(url, headers={"User-Agent": "Kimchi"})
+
+    if request.status_code == 200:
+        soup = BeautifulSoup(request.text, "html.parser")
+        # write your ✨magical✨ code here
+    else:
+        print("Can't get jobs.")
+
+
+extract_jobs("rust")
