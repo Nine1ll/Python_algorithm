f5e27fa2d9a03637db4816f3e4093a57134c5015
GwangYoung <gospotb1607@gmail.com>
2022-09-09 16:31:57 +0900

    임시 파일 commit # TODO1 : remote, wwr 파일에 link 가져올 수 있게 수정필요. # TODO2 : 더욱 세련된 홈페이지 필요 # TODO3 : export 시 이모지 깨지는 것 수정 필요!

--- nomad_coder/main.py
+++ nomad_coder/main.py
@@ -1,13 +1,44 @@
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.wwr import extract_wwr_jobs
 from extractors.remote import extract_remote_jobs
 from file import save_to_file
 
+app = Flask("JobScrapper")
 
-keyword = input("What do you want to search for?: ")
+db = {}
 
-remote = extract_remote_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
-# extractor에 링크 추가 필요!
-jobs = remote + wwr
 
-save_to_file(keyword, jobs)
+@app.route("/")
+def home():
+    return render_template("index.html", name="nine1ll")
+
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        remote = extract_remote_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = remote + wwr
+        db[keyword] = jobs
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
+
+
+app.run("0.0.0.0")
+
